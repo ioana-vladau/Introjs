ccfcda08b8f4525e85cac3e8b1757dba78fbd797
Ioana Vladau <Ioana Vladau>
2017-03-21 11:08:48 +0100

    first javascript example

---
+++ main.js
@@ -0,0 +1,30 @@
+//let name = "Jonas";
+//
+//let numKids = 3;
+//
+//let address = "Blablabej 33";
+//
+//let hair = "Not much";
+//
+//numKids = 4;
+//
+//let age = 38;
+//
+//let info = name + " lives at " + address;
+//
+////console.log(age);
+//age = age+1;
+////console.log(age);
+//
+////console.log(info);
+
+let numChildren = "0";
+let partnerName = "Guy";
+let address = "some address";
+let jobTitle = "development";
+
+let info = "Ioana will have " + numChildren + " children " + "and will be married to " + partnerName + " and will do her " + jobTitle + " at " + address + ".";
+
+console.log(info);
+
+
