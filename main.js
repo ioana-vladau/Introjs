08813805e90055b15ff629f8bbecba61e879b049
Ioana Vladau <Ioana Vladau>
2017-03-21 11:31:40 +0100

    Text moving (addEventListener)

--- main.js
+++ main.js
@@ -1,30 +1,22 @@
-//let name = "Jonas";
-//
-//let numKids = 3;
-//
-//let address = "Blablabej 33";
-//
-//let hair = "Not much";
-//
-//numKids = 4;
-//
-//let age = 38;
-//
-//let info = name + " lives at " + address;
-//
-////console.log(age);
-//age = age+1;
-////console.log(age);
-//
-////console.log(info);
+/*when header is clicked, add a class*/
+let myHeader = document.querySelector("#header");
 
-let numChildren = "0";
-let partnerName = "Guy";
-let address = "some address";
-let jobTitle = "development";
-
-let info = "Ioana will have " + numChildren + " children " + "and will be married to " + partnerName + " and will do her " + jobTitle + " at " + address + ".";
-
-console.log(info);
+myHeader.addEventListener('click', doSomething);
 
 
+function doSomething() {
+//    console.log("clicked");
+//    myHeader.classList.add('awesome');
+//    myHeader.classList.remove('awesome');
+      myHeader.classList.toggle('awesome');
+}
+
+
+
+
+//let myP = document.querySelector("p");
+//console.log(myP);
+//
+//myP.classList.add('awesome');
+//
+//myHeader.classList.add("highlight");
